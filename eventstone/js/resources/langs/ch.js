de8806a59ac37c1296b44bc5e76991d683969679
Uchi Uchibeke <u.i@usask.ca>
2017-01-02 18:07:36 -0500

    Add more chinese langauge

--- eventstone/js/resources/langs/ch.js
+++ eventstone/js/resources/langs/ch.js
@@ -8,5 +8,37 @@ var ch = {
 "衡量您的活动的成本，收益和影响"],
 "logInInfo" : "登录以开始",
 "logOut" : "退出"
+},
+"dashboard" : {
+"summary" : "摘要和统计信息",
+"dTicket" : " 设计入场券",
+"dBadge" : "设计姓名卡",
+"welcome" : "欢迎加入。规划和组织活动",
+"start" : "开始使用",
+"status" : "缺席"
+},
+"live" :  {
+"cInst" : "在下一步骤中，输入入场券或姓名，然后单击签入。",
+"newGQ" : "需要添加新的客人名单吗？",
+"yes" : "确定",
+"no" : "不需要",
+"src" : "查找客人名单",
+"type" : "输入名字",
+"cIn" : "确认出席",
+"status" : "Not Checked In",
+"aNew" : "添加新的客人名单",
+"aAndC" : "添加"
+},
+"nameBadge" : {
+"title" : "设计名牌",
+"content" : "输入",
+"chg" : "添加内容",
+"enter" : "确定",
+"preview" : "预览",
+"totNum" : "需要打印的姓名卡个数为",
+"cAndI" : "请在打印时确认打印的背景颜色和图像",
+"dSided" : "双面打印",
+"sSided" : "单面打印",
+"genAt" : "设计姓名卡"
 }
 };
--- eventstone/js/resources/langs/en.js
+++ eventstone/js/resources/langs/en.js
@@ -8,5 +8,37 @@ var en = {
 "View and manage guestlist on-the-go", "Get event summary and plan for future events", "Measure cost, benefits and impact of your event"],
 "logInInfo" : "Login to get started ",
 "logIn" : "Logout"
+},
+"dashboard" : {
+"summary" : "Summary and Stats",
+"dTicket" : " Design Ticket",
+"dBadge" : "Design Namebadge",
+"welcome" : "Welcome to your dashboard. Add an event to get started",
+"start" : "Login to get started ",
+"status" : "No Show"
+},
+"live" :  {
+"cInst" : "On next page, enter a ticket number or name and click check in.",
+"newGQ" : " Would you want to add new people to the list during checkin?",
+"yes" : "Yes",
+"no" : "No",
+"src" : "Search to checkin",
+"type" : "Please, start typing",
+"cIn" : "Check-In",
+"status" : "Not Checked In",
+"aNew" : "Add New Name and Checkin",
+"aAndC" : "Add and Check in"
+},
+"nameBadge" : {
+"title" : "BADGE DESIGNER",
+"content" : "Content",
+"chg" : "Change the numbers below",
+"enter" : "Enter",
+"preview" : "Preview",
+"totNum" : "The number of badges you will print is",
+"cAndI" : "PLEASE PRINT WITH BACKGROUND COLOR AND IMAGES",
+"dSided" : "Print Double Sided",
+"sSided" : "Print Single Sided",
+"genAt" : "badges designed and generated at"
 }
 };
