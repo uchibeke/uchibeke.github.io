0b9352b399f41c45061d31d78fed63f104ea44c6
Uchi Uchibeke <uchi.u@usask.ca>
2018-03-27 22:47:05 -0600

    Fix errors

--- js/controllers.js
+++ js/controllers.js
@@ -5,10 +5,8 @@ guestControllers.controller('ListController', ['$scope', '$http', '$timeout',
 
 		$scope.data = $scope.data ? $scope.data : {};
 		$scope.actions = $scope.actions ? $scope.actions : {};
-		let
-		d = $scope.data;
-		let
-		a = $scope.actions;
+		var d = $scope.data;
+		var a = $scope.actions;
 
 		d.portfolioItems = [];
 
@@ -54,6 +52,7 @@ guestControllers.controller('ListController', ['$scope', '$http', '$timeout',
 				function initialize() {
 					var feed = new google.feeds.Feed("https://medium.com/feed/@uchi");
 					feed.load(function(result) {
+						d.json = result
 						if (!result.error) {
 							var output = '';
 							var author = result.feed.description.replace("Stories by ", "").replace(" on Medium", "");
@@ -61,24 +60,37 @@ guestControllers.controller('ListController', ['$scope', '$http', '$timeout',
 								output += '<a target="_blank" href="'+ item.link + '">';
 								output += '<div class="col-sm-6 col-md-4 col-xs-12">';
 								output += '<div class="blog-post"><header>';
-								output += '<h6 class="date">' + new Date(item.publishedDate).toLocaleDateString("en-US",dateOptions) + "</h6>";
-							var tagIndex = item.content.indexOf('<img'); // Find where the img tag starts
-							var srcIndex = item.content.substring(tagIndex).indexOf('src="') + tagIndex; // Find where the src attribute starts
-							var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
-							var srcEnd = item.content.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
-							var src = item.content.substring(srcStart, srcEnd); // Extract just the URL
-							output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="100%"></div></header>';
-							output += '<div class="blog-content"><h4><a style="text-overflow:ellipsis;word-wrap:break-word;max-width: inherit;" target="_blank" href="'+ item.link + '">' + item.title + '</a></h4>';
-							output += '<div class="post-meta"><span>By ' + author + '</span></div>';
-							var yourString = item.content.replace(/<img[^>]*>/g,""); //replace with your string.
-							var maxLength = 120 // maximum number of characters to extract
-							//trim the string to the maximum length
-							var trimmedString = yourString.substr(0, maxLength);
-							//re-trim if we are in the middle of a word
-							trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-							output += '<p>' + trimmedString + '...</p>';
-							output += '</div></div></div></a>';
-							return k < 3;
+								output += '<h6 class="date">' + (new Date(item.publishedDate).toLocaleDateString("en-US",dateOptions) || "") + "</h6>";
+								var tagIndex = item.content.indexOf('<img'); // Find where the img tag starts
+								var srcIndex = item.content.substring(tagIndex).indexOf('src="') + tagIndex; // Find where the src attribute starts
+								var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+								var srcEnd = item.content.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+								
+								var div = document.createElement('div');
+								div.innerHTML = item.content;
+								var firstImage = div.getElementsByTagName('img')[0]
+								var imgSrc = firstImage ? firstImage.src : "";
+								// or, if you want the unresolved src, as it appears in the original HTML:
+								var rawImgSrc = firstImage ? firstImage.getAttribute("src") : "";
+								// console.log( rawImgSrc );
+
+
+
+								var src = rawImgSrc//item.content.substring(srcStart, srcEnd); // Extract just the URL
+								output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="100%"></div></header>';
+								output += '<div class="blog-content"><h4><a style="text-overflow:ellipsis;word-wrap:break-word;max-width: inherit;" target="_blank" href="'+ item.link + '">' + item.title + '</a></h4>';
+								output += '<div class="post-meta"><span>By ' + author + '</span></div>';
+								var yourString = div.getElementsByTagName('p')[0].innerText.replace(/<img[^>]*>/g,""); //replace with your string.
+								var maxLength = 120 // maximum number of characters to extract
+								//trim the string to the maximum length
+								var trimmedString = yourString.substr(0, maxLength);
+								//re-trim if we are in the middle of a word
+								trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+								output += '<p id="trimStr">' 
+								output += trimmedString + '...</p>';
+								
+								output += '</div></div></div></a>';
+								return k < 3;
 						});
 							$content.html(output);
 
