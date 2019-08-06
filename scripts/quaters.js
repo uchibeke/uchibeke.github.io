b5acd2af7db28eca7e919735e6e5bf2e589ece46
Uchi Uchibeke <uchi.uchibeke@securekey.com>
2019-08-06 13:33:07 -0400

    Add script

---
+++ scripts/quaters.js
@@ -0,0 +1,206 @@
+const quarters = [
+  {
+    name: "2019 Q2",
+    details: [
+      {
+        title: <h3>Working</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>
+              <li>Exploring a Crowdlending and social credit platform</li>
+              <li>
+                Planning{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://naijahacks.com"
+                >
+                  NaijaHacks 2019
+                </a>
+              </li>
+              <li>
+                Asking the tough questions about long term goals, direction and
+                plans
+              </li>
+              <li>
+                Mentoring the Bio-remote team, one of the winners of{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://twitter.com/search?f=tweets&vertical=default&q=%23naijahacks2018&src=typd"
+                >
+                  #NaijaHacks2018
+                </a>{" "}
+              </li>
+            </ul>
+          </div>
+        )
+      },
+      {
+        title: <h3>Reading</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.goodreads.com/book/show/43078245-risk-and-return"
+                >
+                  Risk and return
+                </a>{" "}
+                by Yomi Jemibewon
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.goodreads.com/book/show/28673347-the-wealthy-renter"
+                >
+                  The Wealthy Renter
+                </a>{" "}
+                by Alex Avery
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/0393318885/"
+                >
+                  Wealth And Poverty Of Nations
+                </a>{" "}
+                by David S Landes
+              </li>
+            </ul>
+          </div>
+        )
+      },
+      {
+        title: <h3>Listening</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B072F85DT1"
+                >
+                  How to Win Friends and Influence People in the Digital Age
+                </a>{" "}
+                by Dale Carnegie & Associates
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B072HSPZ6V"
+                >
+                  The Hard Thing About Hard Things
+                </a>{" "}
+                by Ben Horowitz
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B07B5JTSN9"
+                >
+                  Capital: Volume 1: A Critique of Political Economy
+                </a>{" "}
+                by Karl Marx
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B072HZ5Y3W"
+                >
+                  Soft Skills: The Software Developer's Life Manual
+                </a>{" "}
+                by John Sonmez
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B06Y2H2CJY"
+                >
+                  The Subtle Art of Not Giving a F*ck
+                </a>{" "}
+                by Mark Manson
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.ca/gp/product/B07149J5VB"
+                >
+                  The Intelligent Investor
+                </a>{" "}
+                by Benjamin Graham
+              </li>
+            </ul>
+          </div>
+        )
+      },
+      {
+        title: <h3>Learning</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.lynda.com/Blockchain-tutorials/Blockchain-Beyond-Basics/636127-2.html"
+                >
+                  Blockchain: Beyond the Basics
+                </a>{" "}
+                by Jonathan Reichental
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.lynda.com/course-tutorials/Foundations-Fourth-Industrial-Revolution-Industry-4-0/743170-2.html"
+                >
+                  Foundations of The Fourth Industrial Revolution (Industry 4.0)
+                </a>{" "}
+                by Jonathan Reichental
+              </li>
+            </ul>
+          </div>
+        )
+      },
+      {
+        title: <h3>Travel</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>
+              <li>
+                Q1 - Nigeria,{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="/blog/2019/02/09/visiting-london-during-layover-at-airport-heathrow-airport-gatwich-airport"
+                >
+                  United Kingdown
+                </a>
+              </li>
+              <li>Q2 - USA, Germany, Hungary, Czech Republic</li>
+            </ul>
+          </div>
+        )
+      },
+      {
+        title: <h3>Location</h3>,
+        body: (
+          <div>
+            <ul style={{ paddingLeft: "80px" }}>Right now in Canada</ul>
+          </div>
+        )
+      }
+    ]
+  }
+];
